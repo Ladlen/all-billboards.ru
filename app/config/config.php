78a8c1f25b20e1b4729153910fe60207a29162cb
Dolgov Dmitriy <d.dolgov@i2013.ru>
2016-03-17 06:50:59 +0300

    Добавлен параметр 'side_letter' в файл конфигурации.

--- app/config/config.php
+++ app/config/config.php
@@ -4,5 +4,9 @@ return [
     // Переключение отладочного режима.
     'mode' => 'debug',
     // Ссылка на сайт с адресной программой.
-    'page_path' => 'http://www.ruan.ru/address/6x3/spb/0/'
+    'page_path' => 'http://www.ruan.ru/address/6x3/spb/0/',
+    'side_letter' => [
+        'associations' => ['А' => 'A', 'Б' => 'B'],
+        'exclusive' => 'C'
+    ]
 ];
