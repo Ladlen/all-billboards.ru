3cb7664df1e03096fdae969924de6db945040b6f
Dolgov Dmitriy <d.dolgov@i2013.ru>
2016-03-17 06:03:06 +0300

    Разработка функции scrape().

--- app/helpers/ScrapeBillboards.class.php
+++ app/helpers/ScrapeBillboards.class.php
@@ -16,6 +16,45 @@ class ScrapeBillboards
 
     public function scrape()
     {
+        $doc = new DOMDocument();
+        if (!$doc->loadHTMLFile($this->config['page_path']))
+        {
+            throw new Exception('Не удалось загрузить документ ' . $this->config['page_path']);
+        }
+
+        $host = parse_url($this->config['page_path'], PHP_URL_HOST);
+
+        $xpath = new DOMXpath($doc);
+        $rows = $xpath->query('//form[@name="sel"]/table/tr');
+
+        foreach($rows as $row)
+        {
+            #print_r($row);
+            $td = $row->query('td');
+            print_r($td);
+        }
+        exit;
+
+        foreach($articles as $container)
+        {
+            print_r($container);
+            /*$arr = $container->getElementsByTagName("a");
+            foreach ($arr as $item)
+            {
+                $click = $item->getAttribute("onclick");
+                preg_match("/'+(.*)'+/U", $click, $matches);
+
+                print_r($matches);
+                die();
+
+                #$text = trim(preg_replace("/[\r\n]+/", " ", $item->nodeValue));
+                echo $click . "\n";
+            }*/
+        }
+    }
+
+    protected function getBoardContent($url)
+    {
 
     }
 }
