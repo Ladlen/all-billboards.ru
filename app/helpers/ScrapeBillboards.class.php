c0ac12edb30b0b634346f62682c2d75456206230
Dolgov Dmitriy <d.dolgov@i2013.ru>
2016-03-17 06:51:39 +0300

    Почти завершена функция scrape().

--- app/helpers/ScrapeBillboards.class.php
+++ app/helpers/ScrapeBillboards.class.php
@@ -9,9 +9,15 @@ class ScrapeBillboards
 {
     protected $config;
 
+    /**
+     * @var string хост от url страницы для парсинга
+     */
+    protected $host;
+
     public function __construct($config)
     {
         $this->config = $config;
+        $this->host = parse_url($this->config['page_path'], PHP_URL_HOST);
     }
 
     public function scrape()
@@ -22,20 +28,49 @@ class ScrapeBillboards
             throw new Exception('Не удалось загрузить документ ' . $this->config['page_path']);
         }
 
-        $host = parse_url($this->config['page_path'], PHP_URL_HOST);
-
         $xpath = new DOMXpath($doc);
         $rows = $xpath->query('//form[@name="sel"]/table/tr');
 
-        foreach($rows as $row)
+        foreach ($rows as $row)
         {
-            #print_r($row);
-            $td = $row->query('td');
-            print_r($td);
+            $billboardInfo = [
+                'bb_address' => null,   // Адрес щита
+                'bb_side' => null,      // Сторона щита
+                'bb_link_owner_site' => null, // Ссылка на карточку щита (всплывающее окно с фото и схемой щита)
+                'bb_image' => null,     // Ссылка на фото щита
+                'bb_shema' => null      // Ссылка на схему щита
+            ];
+
+            $cols = $row->getElementsByTagName('td');
+            if ($cols->length == 3)
+            {
+                $billboardInfo['bb_side'] = isset($this->config['side_letter']['associations'][$cols->item(
+                        1
+                    )->textContent]) ?
+                    $this->config['side_letter']['associations'][$cols->item(1)->textContent] :
+                    $this->config['side_letter']['exclusive'];
+
+                $billboardInfo['bb_address'] = $cols->item(2);
+
+                if ($link = $cols->item(2)->getElementsByTagName('a')->item(0))
+                {
+                    $click = $link->getAttribute("onclick");
+                    preg_match("/'+(.*)'+/U", $click, $matches);
+
+                    $url = $this->host . $matches[1];
+                    $billboardInfo['bb_link_owner_site'] = $url;
+
+                    $this->getBoardContent($url, $billboardInfo);
+                }
+            }
+
+            #$xpath = new DOMXpath($row);
+            #$td = $xpath->query('td');
+
         }
         exit;
 
-        foreach($articles as $container)
+        foreach ($articles as $container)
         {
             print_r($container);
             /*$arr = $container->getElementsByTagName("a");
@@ -53,7 +88,7 @@ class ScrapeBillboards
         }
     }
 
-    protected function getBoardContent($url)
+    protected function getBoardContent($url, &$billboardInfo)
     {
 
     }
--- www/index.php
+++ www/index.php
@@ -1,8 +1,3 @@
-<?php
-    error_reporting(E_ALL);
-    ini_set('display_errors', true);
-    ini_set('display_startup_errors', true);
-?>
 <!DOCTYPE html>
 <html>
 <head>
